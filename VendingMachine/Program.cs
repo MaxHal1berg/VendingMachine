2fb3f546c77a1651bb58108d288f2442a70226c5
Max Hallberg <maxhal0710@gmail.com>
2022-12-08 17:15:21 +0100

    Added clothes to the vending machine!

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -8,7 +8,7 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Välkommen till Vending Machine V1.0.0");
+        Console.WriteLine("Välkommen till Vending Machine V1.1.0");
 
         bool loop = true;
 
@@ -66,11 +66,35 @@ internal class Program
                     }
                     break;
             case 4:
-                Console.WriteLine(":V");
-                break;
+                    Clothes pants = new Clothes();
+                    Console.WriteLine("Dina byxor har levererats! Vill du ta på dig dom? Tryck Y om du det, N om du ej vill det");
+                    string byxorSvar = Console.ReadLine();
+
+                    switch (byxorSvar)
+                    {
+                        case "Y":
+                            pants.wearPants();
+                            break;
+                        case "N":
+                            pants.orderPants();
+                            break;
+                    }
+                    break;
             case 5:
-                Console.WriteLine("lmao");
-                break;
+                    Clothes shoes = new Clothes();
+                    Console.WriteLine("Dina skor har levererats! Vill du ta på dig dom? Tryck Y om du det, N om du ej vill det");
+                    string skorSvar = Console.ReadLine();
+
+                    switch (skorSvar)
+                    {
+                        case "Y":
+                            shoes.orderShoes();
+                            break;
+                        case "N":
+                            shoes.wearShoes();
+                            break;
+                    }
+                    break;
             case 6:
                 Console.WriteLine("wot");
                 break;
@@ -93,7 +117,7 @@ internal class Program
             else if (svar == "N")
             {
                 loop = false;
-                Console.Write("Tack för din medverkan! ");
+                Console.Write("Tack för dina beställningar!");
             }
         }
     }
